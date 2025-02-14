c5b28cc26aec477e272654f2f1223044bf1a73b7
DevNand2003 <145089850+DevNand2003@users.noreply.github.com>
2025-02-14 19:25:00 +0530

    Delete billing directory

--- billing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Bill
-
-@admin.register(Bill)
-class BillAdmin(admin.ModelAdmin):
-    list_display = ['invoice_number', 'company', 'total_amount', 'email', 'start_date', 'end_date']
